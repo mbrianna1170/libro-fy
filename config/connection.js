e616e640c2d0fdf839ed4a845d62885e6d1f32ea
Maria Mireles <mireles335@gmail.com>
2021-08-10 22:59:02 -0500

    SELECT 1+1 AS result

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,12 @@
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
-let sequelize;
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  });
-}
+
+// create connection to our db
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+});
+
 module.exports = sequelize;
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// app.use(require('./controllers/'));
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
