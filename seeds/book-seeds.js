10dda84226ad355e7b54995afbc87f83300351e3
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-08-13 19:56:57 -0500

    added book seeds for children books

--- seeds/book-seeds.js
+++ seeds/book-seeds.js
@@ -2,16 +2,16 @@ const { Books } = require('../models');
 
 const categoryData = [
   {
-    book_name: '',
-    author_name: '',
-    book_url: '',
-    category_id: '',
+    book_name: 'Goodnight Moon',
+    author_name: 'Margaret Wise Brown',
+    book_url: 'https://www.hoopladigital.com/title/12293865',
+    category_id: '1',
   },
   {
-    book_name: '',
-    author_name: '',
-    book_url: '',
-    category_id: '',
+    book_name: 'The Little Engine that Could',
+    author_name: 'Watty Piper',
+    book_url: 'https://www.hoopladigital.com/title/12193985',
+    category_id: '1',
   },
   {
     book_name: '',
--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,26 @@
 const seedCategories = require('./category-seeds');
 const seedBooks = require('./book-seeds');
+// const seedUsers = require('./user-seeds');
+// const seedPosts = require('./post-seeds');
 
 const sequelize = require('../config/connection');
 
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    console.log('\n----- DATABASE SYNCED -----\n');
+    await seedCategories();
+    console.log('\n----- CATEGORIES SEEDED -----\n');
+  
+    await seedBooks();
+    console.log('\n----- BOOKS SEEDED -----\n');
+  
+    // await seedUsers();
+    // console.log('\n----- USERS SEEDED -----\n');
+  
+    // await seedPosts();
+    // console.log('\n----- POSTS SEEDED -----\n');
+  
+    process.exit(0);
+  };
 
 seedAll();
