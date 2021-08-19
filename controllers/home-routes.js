30103fb2917d4372acd04292303306791db7de5a
mbrianna1170 <brianna.miller09@yahoo.com>
2021-08-18 21:04:59 -0500

    set up single-post for handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -24,10 +24,61 @@ router.get("/", (req, res) => {
     });
 });
 
-
 // will direct to login and sign-up page
-router.get('/login', (req, res) => {
-  res.render('login');
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+// for signle-post
+router.get("/post/:id", (req, res) => {
+  Book.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: [
+      "id",
+      "book_name",
+      "author_name",
+      "book_url",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No book found with this id" });
+        return;
+      }
+
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      // pass data to template
+      res.render("single-post", { post });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
