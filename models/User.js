08b2f5dc524cd1ebb51a9575b001b319b0d699b1
Maria Mireles <mireles335@gmail.com>
2021-08-18 20:45:24 -0500

    hashpasscode/group

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 // create our User model
 class User extends Model {
   // set up method to run on instance data (per user) to check password
@@ -35,13 +36,20 @@ User.init(
         len: [4]
       }
     }
-},
-{
+  },
+  {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'user'
-},
+  }
 );
 module.exports = User;
