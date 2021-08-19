7ab3ee61ce43c21c999d68862ef9f287eecaaa3d
Maria Mireles <mireles335@gmail.com>
2021-08-18 20:51:21 -0500

    has for PUT

--- models/User.js
+++ models/User.js
@@ -39,11 +39,16 @@ User.init(
   },
   {
     hooks: {
-      // set up beforeCreate lifecycle "hook" functionality
+ 
       async beforeCreate(newUserData) {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
+  
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
     },
     sequelize,
     timestamps: false,
